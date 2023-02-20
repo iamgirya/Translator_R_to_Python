92c313f44b7932235130e25af9f96fa850103774
StarProxima <maks.prozorov1@yandex.ru>
2023-02-20 20:17:47 +0300

    ValueToken

---
+++ lib/lexical_anallyzer/value_tokens.dart
@@ -0,0 +1,23 @@
+import 'package:java_to_csharp_translator/lexical_anallyzer/token.dart';
+
+enum ValueTypeTokens implements Token {
+  int("Ni"),
+  float("Nf"),
+  double("Nd"),
+  string("S"),
+  bool("B");
+
+  @override
+  final String mark;
+  const ValueTypeTokens(this.mark);
+}
+
+class ValueToken {
+  final int id;
+  final ValueTypeTokens type;
+  final dynamic value;
+
+  ValueToken(this.id, this.type, this.value);
+
+  String encode() => "${type.mark}_$id";
+}
