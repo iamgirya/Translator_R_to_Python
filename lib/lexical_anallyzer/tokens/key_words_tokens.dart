06e8f7a35eb8288981dfd9e7166b343a5d6a31a1
StarProxima <maks.prozorov1@yandex.ru>
2023-03-01 22:52:13 +0300

    New key words tokens

--- lib/lexical_anallyzer/tokens/key_words_tokens.dart
+++ lib/lexical_anallyzer/tokens/key_words_tokens.dart
@@ -1,27 +1,60 @@
+// ignore_for_file: constant_identifier_names
+
 import '../../core/extensions.dart';
 import 'token.dart';
 
 enum KeyWordTokens implements Token {
-  class_("class"), // 0
-  if_("if"), // 1
-  else_("else"), // 2
-  for_("for"), // 3
-  while_("while"), // 4
-  integer_("int"), // 5
-  float_("float"), // 6
-  double_("double"), // 7
-  string_("String"), // 8
-  nameSpace_("namespace"), // 9
-  void_("void"), // 10
-  static_("static"), // 11
-  import_("import"), // 12
-  system_("System"), // 13
-  break_("break"), // 14
-  final_("final"), // 15
-  return_("return"), // 16
-  public_("public"), // 17
-  private_("private"), // 18
-  continue_("continue"); // 19
+  abstract_("abstract"),
+  case_("case"),
+  continue_("continue"),
+  extends_("extends"),
+  goto_("goto"),
+  int_("int"),
+  package_("package"),
+  short_("short"),
+  try_("try"),
+  assert_("assert"),
+  catch_("catch"),
+  default_("default"),
+  final_("final"),
+  if_("if"),
+  private_("private"),
+  static_("static"),
+  this_("this"),
+  void_("void"),
+  boolean_("boolean"),
+  char_("char"),
+  do_("do"),
+  long_("long"),
+  protected_("protected"),
+  throw_("throw"),
+  volatile_("volatile"),
+  break_("break"),
+  class_("class"),
+  double_("double"),
+  float_("float"),
+  import_("import"),
+  native_("native"),
+  public_("public"),
+  super_("super"),
+  throws_("throws"),
+  while_("while"),
+  byte_("byte"),
+  const_("const"),
+  else_("else"),
+  for_("for"),
+  instanceof_("instanceof"),
+  new_("new"),
+  return_("return"),
+  switch_("switch"),
+  transient_("transient"),
+  print_("print"),
+  println_("println"),
+  main_("main"),
+  System_("System"),
+  out_("out"),
+  String_("String"),
+  args_("args");
 
   @override
   final String mark;
